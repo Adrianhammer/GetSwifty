81d58c5f525acf2c999f62c17557d519c42b24ee
Adrianhmmr <adrian10hammer@gmail.com>
2024-07-08 22:20:01 +0200

    Initial Commit

---
+++ GetSwifty/GetSwiftyApp.swift
@@ -0,0 +1,17 @@
+//
+//  GetSwiftyApp.swift
+//  GetSwifty
+//
+//  Created by Adrian Hammer on 08/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct GetSwiftyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
